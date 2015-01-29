960ab5869f6c15e67c60a0584e221d6a46bf3e06
Liongold <beimaginativeegroup@gmail.com>
2015-01-29 20:42:16 +0000

    Update

--- frontend/public/js/special_note.js
+++ frontend/public/js/special_note.js
@@ -1,5 +1,6 @@
-console.log("Hello");
+//console.log("Hello");
 window.specialNoteEventListener = function() {
+    //setTimeout(function() { //
     console.log(1);
     console.log($("a#file_click_handler"));
     $("a#file_click_handler").click(function(event) {
@@ -13,9 +14,15 @@ window.specialNoteEventListener = function() {
         return false;
     });
     console.log("hello");
+    if($("a#file_click_handler").length > 0) {
+        alert(1);
+    }else{
+        alert(2);
+    }
+    //}, 5000); // Test code
 };
-if(typeof specialNoteEventListener === "undefined") {
-    alert("error");
-}else{
-    alert("Error");
-}
+//if(typeof specialNoteEventListener === "undefined") {
+//    alert("error");
+//}else{
+//    alert("Error");
+//}
